702d6720ea7916a299f24ea29391a365da126600
DevAndromeda <46562212+DevAndromeda@users.noreply.github.com>
2022-09-23 15:08:26 +0545

    refactor(utils): rename keyMirror to createEnum

--- packages/utils/src/utils.ts
+++ packages/utils/src/utils.ts
@@ -1,7 +1,9 @@
-export function keyMirror<K extends string | number | symbol>(data: K[]) {
+function createEnum<K extends string | number | symbol>(data: K[]) {
     const obj = {} as Record<K, K>;
 
     for (const item of data) obj[item] = item;
 
     return Object.freeze(obj);
 }
+
+export { createEnum, createEnum as keyMirror };
